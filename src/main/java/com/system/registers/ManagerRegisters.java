1a33a45fb782c85503246d0ebea95afa92c76217
devTighadouini <iiliaastighadouini@gmail.com>
2026-04-23 21:16:19 +0200

    ManagerRegisters añadido correctamente y con funcionalidades pedidas

---
+++ src/main/java/com/system/registers/ManagerRegisters.java
@@ -0,0 +1,35 @@
+package com.system.registers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagerRegisters {
+
+    private List<Register> registersList;
+
+    public ManagerRegisters() {
+        this.registersList = generateListDefaultName();
+    }
+
+    public List<Register> getRegistersList() {
+        return registersList;
+    }
+
+    public Register getRegister(String labelName) {
+        return getRegistersList().stream()
+                                 .filter(n -> n.getLabelName().equals(labelName))
+                                 .findFirst()
+                                 .orElseThrow(() -> new IllegalArgumentException("Manager Registers: registro con nombre (" + labelName + "), no encontrado."));
+    }
+
+    private List<Register> generateListDefaultName() {
+        List<Register> result = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            result.add(new Register("R" + i, null));
+        }
+
+        return result;
+    }
+
+}
