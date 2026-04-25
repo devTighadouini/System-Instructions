c1da5acf40a7475538ae0999293b8eb9dcad79e3
devTighadouini <iiliaastighadouini@gmail.com>
2026-04-25 13:28:38 +0200

    Operacion PRINT sobre el sistema realizada correctamente

--- src/main/java/com/system/instructions/system/PrintCommand.java
+++ src/main/java/com/system/instructions/system/PrintCommand.java
@@ -1,23 +1,22 @@
 package com.system.instructions.system;
 
 import com.system.instructions.Action;
+import com.system.log.Log;
 import com.system.registers.ManagerRegisters;
 
 public class PrintCommand extends Action {
-    private ManagerRegisters managerRegisters;
 
     public PrintCommand() {
         super("PRINT");
-        this.managerRegisters = new ManagerRegisters();
     }
 
     @Override
     public void execute() {
-        this.managerRegisters.toLogString();
+        Log.getInstance().add(ManagerRegisters.getInstance().toLogString());
     }
 
     @Override
     public String toLogString() {
-        return "";
+        return "· " + this.getNameAction();
     }
 }
